92da21b94682a880275e8ff533819896c7ae0f3a
babyhoppi <marco.hoppe2@gmx.de>
2014-10-14 09:01:00 +0200

    Test

---
+++ GitDemo/src/gitdemo.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @Author Marco Hoppe
+ */
+public class gitdemo {
+
+	/**
+	 * 
+	 */
+	public gitdemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Das ist ein Git-Workflow-Test");
+	}
+
+}
